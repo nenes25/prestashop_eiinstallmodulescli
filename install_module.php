6e42aa95fc7ff68bec49f97bfc3cee55412f12c9
Hervé <nenes_fr@yahoo.fr>
2015-08-13 12:52:24 +0200

    Optimisation pour pouvoir installer le module avec un lien symbolique

--- install_module.php
+++ install_module.php
@@ -24,7 +24,10 @@
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  http://www.h-hennes.fr/blog/
  */
-require_once dirname(__FILE__).'/../../config/config.inc.php';
+//Gestion du chemin d'inclusion si le module est installé via un lien symbolique
+$basepath = dirname(dirname($_SERVER['SCRIPT_FILENAME'])).DIRECTORY_SEPARATOR;
+
+include_once  $basepath.'../config/config.inc.php';
 require_once dirname(__FILE__).'/eiinstallmodulescli.php';
 
 //Lancemement des actions d'installation
